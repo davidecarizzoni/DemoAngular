05ea4aa2784815c0f6dbcdb13593621af22e5382
davidecarizzoni <davidecarizzoni@gmail.com>
2020-03-16 17:07:51 +0100

    utilizzo pipes e safe navigator

--- src/app/primo/primo.component.ts
+++ src/app/primo/primo.component.ts
@@ -16,6 +16,15 @@ export class PrimoComponent implements OnInit {
   @Input()
   titoloInput:string
 
+  lista = [1,2]
+
+  myDate: Date = new Date();
+
+  oggetto: {chiave: string}
+  oggetto1: {chiave: string} = {
+    chiave:'valore'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
