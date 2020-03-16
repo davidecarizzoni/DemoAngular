7889153dcf2171216a6a9e7a9c8049ee52627bf7
davidecarizzoni <davidecarizzoni@gmail.com>
2020-03-16 17:43:26 +0100

    utilizzo event binding

--- src/app/secondo/secondo.component.ts
+++ src/app/secondo/secondo.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondoComponent implements OnInit {
 
+  testo: string = 'Iniziale';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  clickEvent(pippo: MouseEvent, param:number) {
+   if(param == 2){
+    this.testo="Default";
+   }
+    console.log('Ho cliccato il pulsante '+param);
+    pippo.stopPropagation();
+    //senza questo, cliccando il bottone nel div si prende due click
+  }
+
+  inputEvent(ev){
+    this.testo =ev.targer.value ;
+  }
 }
